c44e9b4b854281b4b8faf656692e76746e8f9439
pavansai1122 <pavansai1122@gmail.com>
2020-02-18 00:51:32 +0530

    updated version and fixed issues

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # imageautocrop
 
-Image Auto cropping having black background & white subject based on opencvjs. Note it is in still development. AutoCrop
+Image Auto cropping having black background & white subject based on opencvjs. Note it is in still development.
+
+## Step 1
+
+import AutoCrop from 'imageautocrop';
+
+## Step 2
+
+const cordinates = AutoCrop.getCropingCordinates(document.querySelector('img'), width, height);
+const cropedBase64 = AutoCrop.cropImage(document.querySelector('img'), cordinates);
+
+# Note
+
+1.This package is based on opencv Contours. So it Crops image having black background & White subject.
+2.This one is still under development. Sometimes it throw error new cv.Mat is not a constructor. Kindly use setTimeout to fixit. In future it will be fixed.
--- main.js
+++ main.js
@@ -1,60 +1,121 @@
 var cv = require("./opencv");
-const getCropingCordinates = (imgElement, width, height) => {
-  const canvas = document.createElement("canvas");
-  canvas.width = width;
-  canvas.height = height;
-  const context = canvas.getContext("2d");
-  context.drawImage(imgElement, 0, 0, width, height);
+class AutoCrop {
+  getCropingCordinates(imgElement, width, height) {
+    const canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    const context = canvas.getContext("2d");
+    context.drawImage(imgElement, 0, 0, width, height);
 
-  // reading canvas image using open cv
-  const src = cv.imread(canvas);
-  const dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
-  cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
-  cv.threshold(src, src, 120, 200, cv.THRESH_BINARY);
-  const contours = new cv.MatVector();
-  const hierarchy = new cv.Mat();
+    // reading canvas image using open cv
+    const src = cv.imread(canvas);
+    const dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
+    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
+    cv.threshold(src, src, 120, 200, cv.THRESH_BINARY);
+    const contours = new cv.MatVector();
+    const hierarchy = new cv.Mat();
 
-  // finding different cordinates using contorus
-  cv.findContours(
-    src,
-    contours,
-    hierarchy,
-    cv.RETR_CCOMP,
-    cv.CHAIN_APPROX_SIMPLE
-  );
-  let maxArea = 0;
-  let cordinates = {};
-  // drwaing and finding biggest cordinates
-  for (let i = 0; i < contours.size(); ++i) {
-    const color = new cv.Scalar(
-      Math.round(Math.random() * 255),
-      Math.round(Math.random() * 255),
-      Math.round(Math.random() * 255)
+    // finding different cordinates using contorus
+    cv.findContours(
+      src,
+      contours,
+      hierarchy,
+      cv.RETR_CCOMP,
+      cv.CHAIN_APPROX_SIMPLE
     );
-    cv.drawContours(dst, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
-    // getting particular boundary
-    const cnt = contours.get(i);
-    const rect = cv.boundingRect(cnt);
-    const area = rect.width * rect.height;
-    // finding max area based on previous value
-    if (area > maxArea) {
-      cordinates = rect;
-      maxArea = area;
+    let maxArea = 0;
+    let cordinates = {};
+    // drwaing and finding biggest cordinates
+    for (let i = 0; i < contours.size(); ++i) {
+      const color = new cv.Scalar(
+        Math.round(Math.random() * 255),
+        Math.round(Math.random() * 255),
+        Math.round(Math.random() * 255)
+      );
+      cv.drawContours(dst, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
+      // getting particular boundary
+      const cnt = contours.get(i);
+      const rect = cv.boundingRect(cnt);
+      const area = rect.width * rect.height;
+      // finding max area based on previous value
+      if (area > maxArea) {
+        cordinates = rect;
+        maxArea = area;
+      }
     }
+    src.delete();
+    dst.delete();
+    contours.delete();
+    hierarchy.delete();
+    return cordinates;
   }
-  src.delete();
-  dst.delete();
-  contours.delete();
-  hierarchy.delete();
-  return cordinates;
-};
-const cropImage = (image, croppingCoords) => {
-  const cc = croppingCoords;
-  const workCan = document.createElement("canvas"); // create a canvas
-  workCan.width = Math.floor(cc.width); // set the canvas resolution to the cropped image size
-  workCan.height = Math.floor(cc.height);
-  const ctx = workCan.getContext("2d"); // get a 2D rendering interface
-  ctx.drawImage(image, -Math.floor(cc.x), -Math.floor(cc.y)); // draw the image offset to place it correctly on the cropped region
-  return workCan.toDataURL();
-};
-module.exports = { getCropingCordinates, cropImage };
+  cropImage(image, croppingCoords) {
+    const cc = croppingCoords;
+    const workCan = document.createElement("canvas"); // create a canvas
+    workCan.width = Math.floor(cc.width); // set the canvas resolution to the cropped image size
+    workCan.height = Math.floor(cc.height);
+    const ctx = workCan.getContext("2d"); // get a 2D rendering interface
+    ctx.drawImage(image, -Math.floor(cc.x), -Math.floor(cc.y)); // draw the image offset to place it correctly on the cropped region
+    return workCan.toDataURL();
+  }
+}
+module.exports = new AutoCrop();
+// const getCropingCordinates = (imgElement, width, height) => {
+//   const canvas = document.createElement("canvas");
+//   canvas.width = width;
+//   canvas.height = height;
+//   const context = canvas.getContext("2d");
+//   context.drawImage(imgElement, 0, 0, width, height);
+
+//   // reading canvas image using open cv
+//   const src = cv.imread(canvas);
+//   const dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
+//   cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
+//   cv.threshold(src, src, 120, 200, cv.THRESH_BINARY);
+//   const contours = new cv.MatVector();
+//   const hierarchy = new cv.Mat();
+
+//   // finding different cordinates using contorus
+//   cv.findContours(
+//     src,
+//     contours,
+//     hierarchy,
+//     cv.RETR_CCOMP,
+//     cv.CHAIN_APPROX_SIMPLE
+//   );
+//   let maxArea = 0;
+//   let cordinates = {};
+//   // drwaing and finding biggest cordinates
+//   for (let i = 0; i < contours.size(); ++i) {
+//     const color = new cv.Scalar(
+//       Math.round(Math.random() * 255),
+//       Math.round(Math.random() * 255),
+//       Math.round(Math.random() * 255)
+//     );
+//     cv.drawContours(dst, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
+//     // getting particular boundary
+//     const cnt = contours.get(i);
+//     const rect = cv.boundingRect(cnt);
+//     const area = rect.width * rect.height;
+//     // finding max area based on previous value
+//     if (area > maxArea) {
+//       cordinates = rect;
+//       maxArea = area;
+//     }
+//   }
+//   src.delete();
+//   dst.delete();
+//   contours.delete();
+//   hierarchy.delete();
+//   return cordinates;
+// };
+// export const cropImage = (image, croppingCoords) => {
+//   const cc = croppingCoords;
+//   const workCan = document.createElement("canvas"); // create a canvas
+//   workCan.width = Math.floor(cc.width); // set the canvas resolution to the cropped image size
+//   workCan.height = Math.floor(cc.height);
+//   const ctx = workCan.getContext("2d"); // get a 2D rendering interface
+//   ctx.drawImage(image, -Math.floor(cc.x), -Math.floor(cc.y)); // draw the image offset to place it correctly on the cropped region
+//   return workCan.toDataURL();
+// };
+// module.exports = { getCropingCordinates, cropImage };
